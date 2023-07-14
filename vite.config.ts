c0ea800170fa64fd8e9a7d006414d5956d81067e
myzsk <myzskdev@gmail.com>
2023-07-14 14:52:27 +0700

    add config import layout

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@layout': fileURLToPath(new URL('./src/layouts', import.meta.url))
     }
   }
 })
