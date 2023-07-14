9cbf152038894c98a4dd2e5ead39a0315c309b15
raihan ardianata <raihan.ardianata@gmail.com>
2023-07-14 17:37:24 +0700

    add responsive

--- src/layouts/LayoutAuth.vue
+++ src/layouts/LayoutAuth.vue
@@ -1,9 +1,10 @@
 <template>
-  <main class="tw-grid tw-grid-cols-2 tw-min-w-full tw-w-full">
-    <div class="tw-col-span-1">
+  <main class="tw-grid tw-grid-cols-2 tw-min-w-full tw-w-full tw-h-screen">
+    <div class="md:tw-col-span-1 tw-col-span-2">
       <router-view />
     </div>
-    <div class="tw-mx-auto tw-col-span-1 flex tw-justify-center tw-items-center tw-w-full tw-bg-auth">
+    <div
+      class="tw-mx-auto md:tw-col-span-1 tw-col-span-2 tw-hidden md:tw-flex tw-justify-center tw-items-center tw-w-full tw-bg-secondary">
       <img src="../assets/images/women.svg" class="tw-aspect-square" />
     </div>
   </main>
